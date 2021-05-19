a19a73fcfb8d6b841a3091360e29046415e1ef55
D DraFz <61958249+ddrafz-mbl@users.noreply.github.com>
2021-05-19 18:55:59 +0700

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,9 @@
 resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
+  cidr_block       = "0.0.0.0/0"
   instance_tenancy = "default"
 
   tags = {
     Name = "demo-vpc"
-    Purpose = "dev-jenkins"
+    Purpose = "demo-jenkins2"
   }
 }
